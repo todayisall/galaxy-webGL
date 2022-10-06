a2fe2b4c95cd73d92494f7d27583b8381a388628
王鹏飞 <todayisall@qq.com>
2022-10-06 21:00:53 +0800

    feat: 第一个demo

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
+import { Application } from "./common/Application";
+import { BasicWebGLApplication } from "./demo/BasicWebGLApplication";
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = ``;
+// 获取用于获得webgl上下文对象的HTMLCanvasElement元素
+let canvas: HTMLCanvasElement | null = document.querySelector(
+  "#webgl"
+) as HTMLCanvasElement;
+
+function frameCallback(app: Application): void {
+  console.log("frameCallback");
+}
+
+function runApplication(): void {
+  let app: Application = new BasicWebGLApplication(canvas);
+  app.frameCallback = frameCallback;
+  app.run();
+}
+
+runApplication();
